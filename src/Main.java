f0e7060c9d87c2308abdfee983ed24e5a759591e
tucker isaacs <isaacstb@mail.uc.edu>
2022-09-15 14:58:09 -0400

    lab 4 task 2

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args)
+    {
+      int costSummer=500;
+      int costFall=550;
+      int costSpring=450;
+      int costWinter=2000;
+
+      int totalCost= costSummer+costWinter+costSpring+costFall;
+
+
+
+
+
+
+        System.out.println("the yearly maintence cost is "+totalCost);
+    }
+}
